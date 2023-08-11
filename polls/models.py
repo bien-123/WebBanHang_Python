78a2b10e1ef7766764220a68f775dfbeb77ccb39
bien-123 <bienle160720@gmail.com>
2023-08-12 01:19:13 +0700

    Bài 2 Model trong django - Model in django

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    time_pub = models.DateTimeField()
+
+
+class Choice(models.Model):
+    # on_delete=models.CASCADE : câu hỏi bị xóa đi thì câu trả lời bị xóa đi
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    vote = models.IntegerField(default=0)

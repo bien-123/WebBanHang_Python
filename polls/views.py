00a8c454392c033d087992ee04cedbebf63f9099
bien-123 <bienle160720@gmail.com>
2023-08-12 01:47:03 +0700

    Bài 4 templates trong Django python part 1

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("HELLO WORLD")
+    # return HttpResponse("HELLO WORLD")
+    myname = "Python"
+    taisan = ["Điện thoại", "Máy tính", "Máy bay"]
+    context = {'name': myname, "taisan": taisan}
+    return render(request, "polls/index.html", context)
+
 
 def abc(request):
     return HttpResponse("PYTHON DJANGO")

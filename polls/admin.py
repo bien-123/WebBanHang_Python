2d6f8b3810211f7263ceb0899dd9f9011779bff2
bien-123 <bienle160720@gmail.com>
2023-08-12 01:29:27 +0700

    Bài 3 Admin site trong Python Django

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Choice, Question
 
 # Register your models here.
+
+# Hiển thị Choice, Question trong Admin
+admin.sites.register(Question)
+admin.sites.register(Choice)

d650819882f8e0854f380ca555f3502f6a6ecbbe
bien-123 <bienle160720@gmail.com>
2023-08-12 16:03:38 +0700

    Bài 9 URL Namespace và static trong django

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('detail/<int:question_id>', views.detailView, name="detailView"),
     path('', views.index, name="index"),

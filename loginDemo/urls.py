44ca863cd3ee4c5ebe47ea763775fc64f787e891
bien-123 <bienle160720@gmail.com>
2023-08-19 14:17:39 +0700

    Bài 17 user login đăng nhập trong django

--- loginDemo/urls.py
+++ loginDemo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import IndexClass
+from .views import IndexClass, LoginClass, ViewUser
 
 
 urlpatterns = [
     path('', IndexClass.as_view(), name="index"),
-
+    path('login/', LoginClass.as_view(), name="login"),
+    path('user/', ViewUser.as_view(), name="user"),
 ]
--- loginDemo/views.py
+++ loginDemo/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render, HttpResponse
 from django.views import View
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 class IndexClass(View):
     def get(self, request):
-        return HttpResponse('<h1>Xin chao</h1>')
+        return HttpResponse('<h1>Xin chào</h1>')
+
+
+class LoginClass(View):
+    def get(self, request):
+        return render(request, 'demo/login.html')
+
+    def post(self, request):
+        user_name = request.POST.get('tendangnhap')
+        pass_word = request.POST.get('password')
+        my_user = authenticate(username=user_name, password=pass_word)
+        if my_user is None:
+            return HttpResponse('Dang nhap that bai! User khong ton tai')
+
+        login(request, my_user)
+        return render(request, 'demo/success.html')
+
+class ViewUser(View):
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return HttpResponse('Ban vui long dang nhap')
+        else:
+            return HttpResponse('<h1>Day la view user</h1>')
